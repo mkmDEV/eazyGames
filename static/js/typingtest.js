1ce9d27b7312e82d53e347127291f4b0cf311138
Kristóf Mátyás Marsi <mkmdev18@gmail.com>
2019-01-02 15:12:33 +0100

    hide, show start and stop buttons

---
+++ static/js/typingtest.js
@@ -0,0 +1,10 @@
+// BUTTONS
+document.getElementById('start-button').addEventListener('click', function () {
+    document.getElementById('start-button').style.display="none";
+    document.getElementById('stop-button').style.display="block";
+});
+
+document.getElementById('stop-button').addEventListener('click', function () {
+    document.getElementById('stop-button').style.display="none";
+    document.getElementById('start-button').style.display="block";
+});
