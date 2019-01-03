b783ad0ab563f70d753b4dc5b5f40308b3039f79
Kristóf Mátyás Marsi <mkmdev18@gmail.com>
2019-01-03 15:05:53 +0100

    add place for timer

--- static/js/typingtest.js
+++ static/js/typingtest.js
@@ -3,8 +3,12 @@ let sentences = [
                 "Az ipafai papnak fapipája van, ezért az ibafai fapipa papi fapipa.",
                 "Egy meggymag, meg még egy meggymag az két meggymag.",
                 "Öt görög görget görgőn, görbe úton, görgő gömbbé gömbölyödött öt görögdinnyét.",
+//    TODO: randomot finomítani, hogy kattintásra ne az előző mondat jelenjen meg!
 ];
 
+let startTime = '';
+let stopTime = '';
+let elapsedTime = '';
 
 // BUTTONS
 document.getElementById('start-button').addEventListener('click', function () {
@@ -14,11 +18,19 @@ document.getElementById('start-button').addEventListener('click', function () {
     document.getElementById('sentence').placeholder=sentences[Math.floor(Math.random()*sentences.length)];
     document.getElementById('answer').value='';
     document.getElementById('answer').focus();
+    startTime = getMyTime();
 });
 
 document.getElementById('stop-button').addEventListener('click', function () {
     document.getElementById('stop-button').style.display="none";
     document.getElementById('start-button').style.display="block";
+    stopTime = getMyTime();
+    elapsedTime = startTime-startTime;
+    console.log(elapsedTime)
 });
 
 
+function getMyTime (){
+    theTime = new Date().getTime();
+    console.log(theTime)
+}
