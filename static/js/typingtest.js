49ad7b4cad461802f3bae7475e070e04db85e760
Kristóf Mátyás Marsi <mkmdev18@gmail.com>
2019-01-03 13:54:32 +0100

    added random sentences

--- static/js/typingtest.js
+++ static/js/typingtest.js
@@ -1,12 +1,22 @@
+let sentences = [
+                "Fekete bikapata kopog a patika pepita kövén.",
+                "Az ipafai papnak fapipája van, ezért az ibafai fapipa papi fapipa.",
+                "Egy meggymag, meg még egy meggymag az két meggymag.",
+                "Öt görög görget görgőn, görbe úton, görgő gömbbé gömbölyödött öt görögdinnyét.",
+];
+
+
 // BUTTONS
 document.getElementById('start-button').addEventListener('click', function () {
     document.getElementById('start-button').style.display="none";
     document.getElementById('stop-button').classList.remove('d-none');
     document.getElementById('stop-button').style.display="block";
+    document.getElementById('sentence').placeholder=sentences[Math.floor(Math.random()*sentences.length)];
 });
 
 document.getElementById('stop-button').addEventListener('click', function () {
     document.getElementById('stop-button').style.display="none";
     document.getElementById('start-button').style.display="block";
 });
+
 
