a600efac522f63112c902d71058b90d4ffdbd305
Kristóf Mátyás Marsi <mkmdev18@gmail.com>
2019-01-03 11:14:16 +0100

    fixed buttons switch

--- static/js/typingtest.js
+++ static/js/typingtest.js
@@ -1,6 +1,7 @@
 // BUTTONS
 document.getElementById('start-button').addEventListener('click', function () {
     document.getElementById('start-button').style.display="none";
+    document.getElementById('stop-button').classList.remove('d-none');
     document.getElementById('stop-button').style.display="block";
 });
 
@@ -8,3 +9,4 @@ document.getElementById('stop-button').addEventListener('click', function () {
     document.getElementById('stop-button').style.display="none";
     document.getElementById('start-button').style.display="block";
 });
+
